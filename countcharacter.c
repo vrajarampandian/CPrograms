cf6918512574dab4ac2dfecf991eaf7acce73b21
Rajaram Pandian <vrajarampandian@gmail.com>
2022-10-14 22:50:31 +0530

    count the character using

---
+++ countcharacter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    char j = '1';
+    for(j = '1';j <='9';j++)
+        putchar(j);
+    //printf("%c\n", j);
+    /*long nc;
+    nc = 0;
+    /*while(getchar() != EOF)
+    {
+        ++nc;
+    }
+    printf("Count %ld\n", nc);*/
+}
